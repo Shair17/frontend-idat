d6fddf039792f476ee41a52ff602e65590c3aabf
Shair17 <jimmy_jhair17@hotmail.com>
2023-07-08 21:24:11 -0500

    Automatizando productos con javascript (funciones, arreglos y objetos)

--- main.js
+++ main.js
@@ -1,90 +1,348 @@
 import './style.css';
 
-let institucion = 'IDAT';
-let nombre = 'Carlos';
-const edad = 15;
-console.log(
-	`Hola Mundo, soy ${nombre} y tengo ${edad} años, estudio en ${institucion}`
-);
+const productos = [
+	// Una fila
+	[
+		{
+			imagen: 'https://picsum.photos/400',
+			precio: 6.99,
+			precioAnterior: 12.99,
+			cantidadVendidos: 500,
+			calificacion: 4.8,
+			titulo:
+				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
+			tieneEnvioGratis: false,
+		},
+		{
+			imagen: 'https://picsum.photos/400',
+			precio: 6.99,
+			precioAnterior: 12.99,
+			cantidadVendidos: 500,
+			calificacion: 4.8,
+			titulo:
+				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
+			tieneEnvioGratis: false,
+		},
+		{
+			imagen: 'https://picsum.photos/400',
+			precio: 6.99,
+			precioAnterior: 12.99,
+			cantidadVendidos: 500,
+			calificacion: 4.8,
+			titulo:
+				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
+			tieneEnvioGratis: false,
+		},
+		{
+			imagen: 'https://picsum.photos/400',
+			precio: 6.99,
+			precioAnterior: 12.99,
+			cantidadVendidos: 500,
+			calificacion: 4.8,
+			titulo:
+				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
+			tieneEnvioGratis: false,
+		},
+		{
+			imagen: 'https://picsum.photos/400',
+			precio: 6.99,
+			precioAnterior: 12.99,
+			cantidadVendidos: 500,
+			calificacion: 4.8,
+			titulo:
+				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
+			tieneEnvioGratis: false,
+		},
+		{
+			imagen: 'https://picsum.photos/400',
+			precio: 6.99,
+			precioAnterior: 12.99,
+			cantidadVendidos: 500,
+			calificacion: 4.8,
+			titulo:
+				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
+			tieneEnvioGratis: false,
+		},
+	],
 
-let precioArroz = 3;
+	// Otra fila
+	[
+		{
+			imagen: 'https://picsum.photos/400',
+			precio: 6.99,
+			precioAnterior: 12.99,
+			cantidadVendidos: 500,
+			calificacion: 4.8,
+			titulo:
+				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
+			tieneEnvioGratis: false,
+		},
+		{
+			imagen: 'https://picsum.photos/400',
+			precio: 6.99,
+			precioAnterior: 12.99,
+			cantidadVendidos: 500,
+			calificacion: 4.8,
+			titulo:
+				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
+			tieneEnvioGratis: false,
+		},
+		{
+			imagen: 'https://picsum.photos/400',
+			precio: 6.99,
+			precioAnterior: 12.99,
+			cantidadVendidos: 500,
+			calificacion: 4.8,
+			titulo:
+				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
+			tieneEnvioGratis: false,
+		},
+		{
+			imagen: 'https://picsum.photos/400',
+			precio: 6.99,
+			precioAnterior: 12.99,
+			cantidadVendidos: 500,
+			calificacion: 4.8,
+			titulo:
+				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
+			tieneEnvioGratis: false,
+		},
+		{
+			imagen: 'https://picsum.photos/400',
+			precio: 6.99,
+			precioAnterior: 12.99,
+			cantidadVendidos: 500,
+			calificacion: 4.8,
+			titulo:
+				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
+			tieneEnvioGratis: false,
+		},
+		{
+			imagen: 'https://picsum.photos/400',
+			precio: 6.99,
+			precioAnterior: 12.99,
+			cantidadVendidos: 500,
+			calificacion: 4.8,
+			titulo:
+				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
+			tieneEnvioGratis: false,
+		},
+	],
 
-if (precioArroz <= 4) {
-	console.log('ok puedes comprar el arroz porque está 4 soles o menos');
-} else {
-	console.log('el arroz está más de 4 soles');
+	// Otra fila
+	[
+		{
+			imagen: 'https://picsum.photos/400',
+			precio: 6.99,
+			precioAnterior: 12.99,
+			cantidadVendidos: 500,
+			calificacion: 4.8,
+			titulo:
+				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
+			tieneEnvioGratis: false,
+		},
+		{
+			imagen: 'https://picsum.photos/400',
+			precio: 6.99,
+			precioAnterior: 12.99,
+			cantidadVendidos: 500,
+			calificacion: 4.8,
+			titulo:
+				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
+			tieneEnvioGratis: false,
+		},
+		{
+			imagen: 'https://picsum.photos/400',
+			precio: 6.99,
+			precioAnterior: 12.99,
+			cantidadVendidos: 500,
+			calificacion: 4.8,
+			titulo:
+				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
+			tieneEnvioGratis: false,
+		},
+		{
+			imagen: 'https://picsum.photos/400',
+			precio: 6.99,
+			precioAnterior: 12.99,
+			cantidadVendidos: 500,
+			calificacion: 4.8,
+			titulo:
+				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
+			tieneEnvioGratis: false,
+		},
+		{
+			imagen: 'https://picsum.photos/400',
+			precio: 6.99,
+			precioAnterior: 12.99,
+			cantidadVendidos: 500,
+			calificacion: 4.8,
+			titulo:
+				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
+			tieneEnvioGratis: false,
+		},
+		{
+			imagen: 'https://picsum.photos/400',
+			precio: 6.99,
+			precioAnterior: 12.99,
+			cantidadVendidos: 500,
+			calificacion: 4.8,
+			titulo:
+				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
+			tieneEnvioGratis: false,
+		},
+	],
+
+	// Otra fila
+	[
+		{
+			imagen: 'https://picsum.photos/400',
+			precio: 6.99,
+			precioAnterior: 12.99,
+			cantidadVendidos: 500,
+			calificacion: 4.8,
+			titulo:
+				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
+			tieneEnvioGratis: false,
+		},
+		{
+			imagen: 'https://picsum.photos/400',
+			precio: 6.99,
+			precioAnterior: 12.99,
+			cantidadVendidos: 500,
+			calificacion: 4.8,
+			titulo:
+				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
+			tieneEnvioGratis: false,
+		},
+		{
+			imagen: 'https://picsum.photos/400',
+			precio: 6.99,
+			precioAnterior: 12.99,
+			cantidadVendidos: 500,
+			calificacion: 4.8,
+			titulo:
+				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
+			tieneEnvioGratis: false,
+		},
+		{
+			imagen: 'https://picsum.photos/400',
+			precio: 6.99,
+			precioAnterior: 12.99,
+			cantidadVendidos: 500,
+			calificacion: 4.8,
+			titulo:
+				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
+			tieneEnvioGratis: false,
+		},
+		{
+			imagen: 'https://picsum.photos/400',
+			precio: 6.99,
+			precioAnterior: 12.99,
+			cantidadVendidos: 500,
+			calificacion: 4.8,
+			titulo:
+				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
+			tieneEnvioGratis: false,
+		},
+		{
+			imagen: 'https://picsum.photos/400',
+			precio: 6.99,
+			precioAnterior: 12.99,
+			cantidadVendidos: 500,
+			calificacion: 4.8,
+			titulo:
+				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
+			tieneEnvioGratis: false,
+		},
+	],
+];
+
+function $(elemento) {
+	return document.querySelector(elemento);
 }
 
-// supongamos que quieres ingresar a una universidad donde solo permiten a mayores de edad
-// y además tienes que haber estudiado en IDAT
-if (edad >= 18 && institucion === 'IDAT') {
-	console.log('ok puedes ingresar');
-} else {
-	console.log('no puedes ingresar');
+const container = $('.container');
+
+function construirGrilla() {
+	let resultado = '';
+
+	for (let i = 0; i < productos.length; i++) {
+		const noEsPrimeraFila = i > 0;
+
+		const filaDeProductos = productos[i];
+		const fila = document.createElement('div');
+		fila.className = noEsPrimeraFila ? `row mt-4` : `row`;
+
+		for (let j = 0; j < filaDeProductos.length; j++) {
+			const producto = filaDeProductos[j];
+			const col = document.createElement('div');
+			col.className = 'col-md-2';
+			const productoContainer = document.createElement('a');
+			productoContainer.href = '#';
+			productoContainer.className = 'rounded producto-container shadow-sm';
+			const imagen = document.createElement('img');
+			imagen.className = 'producto-imagen';
+			imagen.src = `${producto.imagen}?${i}-${j}`;
+			imagen.alt = 'Imagen de Producto';
+			const productoItem = document.createElement('div');
+			productoItem.className = 'producto-item p-2';
+
+			// Precio
+			const precioDiv = document.createElement('div');
+			const precioMoneda = document.createElement('span');
+			precioMoneda.className = 'fs-6 fw-semibold';
+			precioMoneda.textContent = 'PEN';
+			const precioActual = document.createElement('span');
+			precioActual.className = 'fw-bold fs-4';
+			precioActual.textContent = producto.precio;
+			const precioAnterior = document.createElement('span');
+			precioAnterior.className =
+				'text-body-tertiary text-decoration-line-through';
+			precioAnterior.textContent = 'PEN ' + producto.precioAnterior;
+			precioDiv.appendChild(precioMoneda);
+			precioDiv.appendChild(precioActual);
+			precioDiv.appendChild(precioAnterior);
+
+			// Cantidad vendidos y calificación
+			const infoDiv = document.createElement('div');
+			const cantidadVendidos = document.createElement('span');
+			cantidadVendidos.className = 'text-secondary';
+			cantidadVendidos.textContent = producto.cantidadVendidos + '+ vendido(s)';
+			const calificacion = document.createElement('span');
+			calificacion.className = 'text-secondary';
+			calificacion.style.marginLeft = '5px';
+			calificacion.textContent = '★ ' + producto.calificacion;
+			infoDiv.appendChild(cantidadVendidos);
+			infoDiv.appendChild(calificacion);
+
+			// Título del producto
+			const titulo = document.createElement('h2');
+			titulo.className = 'text-truncate producto-name h6';
+			titulo.style.maxWidth = '180px';
+			titulo.title = producto.titulo;
+			titulo.textContent = producto.titulo;
+
+			// Envío gratis
+			const envioGratis = document.createElement('span');
+			envioGratis.className = 'producto-envio';
+			envioGratis.textContent = 'Envío gratis';
+
+			productoItem.appendChild(precioDiv);
+			productoItem.appendChild(infoDiv);
+			productoItem.appendChild(titulo);
+			productoItem.appendChild(envioGratis);
+
+			productoContainer.appendChild(imagen);
+			productoContainer.appendChild(productoItem);
+			col.appendChild(productoContainer);
+			fila.appendChild(col);
+		}
+
+		resultado += fila.outerHTML;
+	}
+
+	return resultado;
 }
 
-if (edad >= 18 || institucion === 'IDAT') {
-	console.log(
-		'ok puedes ingresar porque o tienes 18 años o has estudiado en IDAT'
-	);
-}
-
-let numeroDeLaSuerte = '17';
-
-if (numeroDeLaSuerte == 17) {
-	console.log('PRIMERO', 'que bien! el numero de la suerte es 17');
-} else {
-	console.log('PRIMERO', 'el numero de la suerte no es 17');
-}
-
-if (numeroDeLaSuerte === 17) {
-	console.log('SEGUNDO', 'que bien! el numero de la suerte es 17');
-} else {
-	console.log('SEGUNDO', 'el numero de la suerte no es 17');
-}
-
-if (numeroDeLaSuerte != 18) {
-	console.log('TERCERO', 'el numero si es diferente de 18');
-} else {
-	console.log('TERCERO', 'el numero no es diferente de 18');
-}
-
-if (numeroDeLaSuerte !== 18) {
-	console.log('CUARTO', 'el numero si es diferente de 18');
-} else {
-	console.log('CUARTO', 'el numero no es diferente de 18');
-}
-
-for (let index = 0; index < 10; index++) {
-	console.log('Hola!', index);
-}
-
-let contador = 5;
-
-while (contador >= 1) {
-	console.log('Hola!', contador);
-	contador--;
-}
-
-console.log('2 elevado al 8', 2 ** 8);
-
-console.log(124 % 12);
-
-if (92 % 2 === 0) {
-	console.log('92 es numero par');
-}
-
-const mayorDeEdad = edad >= 18;
-
-console.log('mayorDeEdad', mayorDeEdad);
-
-if (!mayorDeEdad) {
-	console.log('ok tienes una beca por ser menor de edad');
-} else {
-	console.log('lo siento, no tienes una beca porque eres mayor de edad');
-}
-
-console.log(
-	'Si eres mayor de edad tomas coca cola, pero si eres menor de edad tomas inka cola. Resultado:',
-	mayorDeEdad ? 'coca cola' : 'inka cola'
-);
+container.innerHTML = construirGrilla();
