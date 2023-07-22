cadbd2e84618afe3d68cfd47d2748717fae8ef7e
Shair17 <jimmy_jhair17@hotmail.com>
2023-07-22 16:51:07 -0500

    localStorage

--- main.js
+++ main.js
@@ -4,363 +4,42 @@ function $(elemento) {
 	return document.querySelector(elemento);
 }
 
-const containerPersonajes = $('#containerPersonajes');
-
-// async - await
-async function llamarApi() {
-	const respuesta = await fetch('https://rickandmortyapi.com/api/character');
-	const datos = await respuesta.json();
-
-	return datos.results;
+// Limpiamos toda la información del localStorage
+function limpiarLocalStorage() {
+	window.localStorage.clear();
+	console.log('El localStorage ha sido limpiado');
 }
 
-llamarApi().then((personajes) => {
-	// pintar los datos de la api en pantalla
-	// usar el id #containerPersonajes
-	// usar la constante ´containerPersonajes´
-	// ...
+// usamos una función para guardar información en el localStorage
+function guardarDatosEnElLocalStorage(key, value) {
+	window.localStorage.setItem(key, value);
+}
+
+// obtenemos información de una key del localStorage
+function obtenerDatosDelLocalStorage(key) {
+	return window.localStorage.getItem(key);
+}
+
+// eliminamos datos del localStorage basadados en la key
+function eliminarDatosDelLocalStorage(key) {
+	return window.localStorage.removeItem(key);
+}
+
+const key = 'numero';
+
+limpiarLocalStorage();
+
+const stringify = JSON.stringify({
+	nombre: 'Juan',
+	edad: 20,
+	instituto: 'IDAT',
 });
 
-//
-const productos = [
-	// Una fila
-	[
-		{
-			imagen: 'https://picsum.photos/400',
-			precio: 6.99,
-			precioAnterior: 12.99,
-			cantidadVendidos: 500,
-			calificacion: 4.8,
-			titulo:
-				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
-			tieneEnvioGratis: false,
-		},
-		{
-			imagen: 'https://picsum.photos/400',
-			precio: 6.99,
-			precioAnterior: 12.99,
-			cantidadVendidos: 500,
-			calificacion: 4.8,
-			titulo:
-				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
-			tieneEnvioGratis: false,
-		},
-		{
-			imagen: 'https://picsum.photos/400',
-			precio: 6.99,
-			precioAnterior: 12.99,
-			cantidadVendidos: 500,
-			calificacion: 4.8,
-			titulo:
-				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
-			tieneEnvioGratis: false,
-		},
-		{
-			imagen: 'https://picsum.photos/400',
-			precio: 6.99,
-			precioAnterior: 12.99,
-			cantidadVendidos: 500,
-			calificacion: 4.8,
-			titulo:
-				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
-			tieneEnvioGratis: false,
-		},
-		{
-			imagen: 'https://picsum.photos/400',
-			precio: 6.99,
-			precioAnterior: 12.99,
-			cantidadVendidos: 500,
-			calificacion: 4.8,
-			titulo:
-				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
-			tieneEnvioGratis: false,
-		},
-		{
-			imagen: 'https://picsum.photos/400',
-			precio: 6.99,
-			precioAnterior: 12.99,
-			cantidadVendidos: 500,
-			calificacion: 4.8,
-			titulo:
-				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
-			tieneEnvioGratis: false,
-		},
-	],
+guardarDatosEnElLocalStorage(key, stringify);
 
-	// Otra fila
-	[
-		{
-			imagen: 'https://picsum.photos/400',
-			precio: 6.99,
-			precioAnterior: 12.99,
-			cantidadVendidos: 500,
-			calificacion: 4.8,
-			titulo:
-				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
-			tieneEnvioGratis: false,
-		},
-		{
-			imagen: 'https://picsum.photos/400',
-			precio: 6.99,
-			precioAnterior: 12.99,
-			cantidadVendidos: 500,
-			calificacion: 4.8,
-			titulo:
-				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
-			tieneEnvioGratis: false,
-		},
-		{
-			imagen: 'https://picsum.photos/400',
-			precio: 6.99,
-			precioAnterior: 12.99,
-			cantidadVendidos: 500,
-			calificacion: 4.8,
-			titulo:
-				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
-			tieneEnvioGratis: false,
-		},
-		{
-			imagen: 'https://picsum.photos/400',
-			precio: 6.99,
-			precioAnterior: 12.99,
-			cantidadVendidos: 500,
-			calificacion: 4.8,
-			titulo:
-				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
-			tieneEnvioGratis: false,
-		},
-		{
-			imagen: 'https://picsum.photos/400',
-			precio: 6.99,
-			precioAnterior: 12.99,
-			cantidadVendidos: 500,
-			calificacion: 4.8,
-			titulo:
-				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
-			tieneEnvioGratis: false,
-		},
-		{
-			imagen: 'https://picsum.photos/400',
-			precio: 6.99,
-			precioAnterior: 12.99,
-			cantidadVendidos: 500,
-			calificacion: 4.8,
-			titulo:
-				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
-			tieneEnvioGratis: false,
-		},
-	],
+console.log('ahora si está');
+console.log(JSON.parse(obtenerDatosDelLocalStorage(key)));
 
-	// Otra fila
-	[
-		{
-			imagen: 'https://picsum.photos/400',
-			precio: 6.99,
-			precioAnterior: 12.99,
-			cantidadVendidos: 500,
-			calificacion: 4.8,
-			titulo:
-				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
-			tieneEnvioGratis: false,
-		},
-		{
-			imagen: 'https://picsum.photos/400',
-			precio: 6.99,
-			precioAnterior: 12.99,
-			cantidadVendidos: 500,
-			calificacion: 4.8,
-			titulo:
-				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
-			tieneEnvioGratis: false,
-		},
-		{
-			imagen: 'https://picsum.photos/400',
-			precio: 6.99,
-			precioAnterior: 12.99,
-			cantidadVendidos: 500,
-			calificacion: 4.8,
-			titulo:
-				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
-			tieneEnvioGratis: false,
-		},
-		{
-			imagen: 'https://picsum.photos/400',
-			precio: 6.99,
-			precioAnterior: 12.99,
-			cantidadVendidos: 500,
-			calificacion: 4.8,
-			titulo:
-				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
-			tieneEnvioGratis: false,
-		},
-		{
-			imagen: 'https://picsum.photos/400',
-			precio: 6.99,
-			precioAnterior: 12.99,
-			cantidadVendidos: 500,
-			calificacion: 4.8,
-			titulo:
-				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
-			tieneEnvioGratis: false,
-		},
-		{
-			imagen: 'https://picsum.photos/400',
-			precio: 6.99,
-			precioAnterior: 12.99,
-			cantidadVendidos: 500,
-			calificacion: 4.8,
-			titulo:
-				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
-			tieneEnvioGratis: false,
-		},
-	],
-
-	// Otra fila
-	[
-		{
-			imagen: 'https://picsum.photos/400',
-			precio: 6.99,
-			precioAnterior: 12.99,
-			cantidadVendidos: 500,
-			calificacion: 4.8,
-			titulo:
-				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
-			tieneEnvioGratis: false,
-		},
-		{
-			imagen: 'https://picsum.photos/400',
-			precio: 6.99,
-			precioAnterior: 12.99,
-			cantidadVendidos: 500,
-			calificacion: 4.8,
-			titulo:
-				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
-			tieneEnvioGratis: false,
-		},
-		{
-			imagen: 'https://picsum.photos/400',
-			precio: 6.99,
-			precioAnterior: 12.99,
-			cantidadVendidos: 500,
-			calificacion: 4.8,
-			titulo:
-				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
-			tieneEnvioGratis: false,
-		},
-		{
-			imagen: 'https://picsum.photos/400',
-			precio: 6.99,
-			precioAnterior: 12.99,
-			cantidadVendidos: 500,
-			calificacion: 4.8,
-			titulo:
-				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
-			tieneEnvioGratis: false,
-		},
-		{
-			imagen: 'https://picsum.photos/400',
-			precio: 6.99,
-			precioAnterior: 12.99,
-			cantidadVendidos: 500,
-			calificacion: 4.8,
-			titulo:
-				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
-			tieneEnvioGratis: false,
-		},
-		{
-			imagen: 'https://picsum.photos/400',
-			precio: 6.99,
-			precioAnterior: 12.99,
-			cantidadVendidos: 500,
-			calificacion: 4.8,
-			titulo:
-				'Gafas de sol modernas para usarlas en la playa con protección contra rayos UV',
-			tieneEnvioGratis: false,
-		},
-	],
-];
-
-const container = $('#containerProductos');
-
-function construirGrilla() {
-	let resultado = '';
-
-	for (let i = 0; i < productos.length; i++) {
-		const noEsPrimeraFila = i > 0;
-
-		const filaDeProductos = productos[i];
-		const fila = document.createElement('div');
-		fila.className = noEsPrimeraFila ? `row mt-4` : `row`;
-
-		for (let j = 0; j < filaDeProductos.length; j++) {
-			const producto = filaDeProductos[j];
-			const col = document.createElement('div');
-			col.className = 'col-md-2';
-			const productoContainer = document.createElement('a');
-			productoContainer.href = '#';
-			productoContainer.className = 'rounded producto-container shadow-sm';
-			const imagen = document.createElement('img');
-			imagen.className = 'producto-imagen';
-			imagen.src = `${producto.imagen}?${i}-${j}`;
-			imagen.alt = 'Imagen de Producto';
-			const productoItem = document.createElement('div');
-			productoItem.className = 'producto-item p-2';
-
-			// Precio
-			const precioDiv = document.createElement('div');
-			const precioMoneda = document.createElement('span');
-			precioMoneda.className = 'fs-6 fw-semibold';
-			precioMoneda.textContent = 'PEN';
-			const precioActual = document.createElement('span');
-			precioActual.className = 'fw-bold fs-4';
-			precioActual.textContent = producto.precio;
-			const precioAnterior = document.createElement('span');
-			precioAnterior.className =
-				'text-body-tertiary text-decoration-line-through';
-			precioAnterior.textContent = 'PEN ' + producto.precioAnterior;
-			precioDiv.appendChild(precioMoneda);
-			precioDiv.appendChild(precioActual);
-			precioDiv.appendChild(precioAnterior);
-
-			// Cantidad vendidos y calificación
-			const infoDiv = document.createElement('div');
-			const cantidadVendidos = document.createElement('span');
-			cantidadVendidos.className = 'text-secondary';
-			cantidadVendidos.textContent = producto.cantidadVendidos + '+ vendido(s)';
-			const calificacion = document.createElement('span');
-			calificacion.className = 'text-secondary';
-			calificacion.style.marginLeft = '5px';
-			calificacion.textContent = '★ ' + producto.calificacion;
-			infoDiv.appendChild(cantidadVendidos);
-			infoDiv.appendChild(calificacion);
-
-			// Título del producto
-			const titulo = document.createElement('h2');
-			titulo.className = 'text-truncate producto-name h6';
-			titulo.style.maxWidth = '180px';
-			titulo.title = producto.titulo;
-			titulo.textContent = producto.titulo;
-
-			// Envío gratis
-			const envioGratis = document.createElement('span');
-			envioGratis.className = 'producto-envio';
-			envioGratis.textContent = 'Envío gratis';
-
-			productoItem.appendChild(precioDiv);
-			productoItem.appendChild(infoDiv);
-			productoItem.appendChild(titulo);
-			productoItem.appendChild(envioGratis);
-
-			productoContainer.appendChild(imagen);
-			productoContainer.appendChild(productoItem);
-			col.appendChild(productoContainer);
-			fila.appendChild(col);
-		}
-
-		resultado += fila.outerHTML;
-	}
-
-	return resultado;
-}
-
-container.innerHTML = construirGrilla();
+console.log('ahora no está');
+eliminarDatosDelLocalStorage(key);
+console.log(JSON.parse(obtenerDatosDelLocalStorage(key)));
