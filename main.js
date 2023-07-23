dab3557bb0d97cd4586cd93fd2ced75c224ffe9e
Shair17 <jimmy_jhair17@hotmail.com>
2023-07-22 20:56:20 -0500

    Hackaton: Resuelto

--- main.js
+++ main.js
@@ -12,14 +12,8 @@ function limpiarLocalStorage() {
 }
 
 // usamos una función para guardar información en el localStorage
-function guardarDatosEnElLocalStorage(key, value) {
-	window.localStorage.setItem(key, value);
-}
 
 // obtenemos información de una key del localStorage
-function obtenerDatosDelLocalStorage(key) {
-	return window.localStorage.getItem(key);
-}
 
 // eliminamos datos del localStorage basadados en la key
 function eliminarDatosDelLocalStorage(key) {
@@ -30,19 +24,6 @@ function eliminarDatosDelLocalStorage(key) {
 //
 //
 //
-// Modificar el texto del H1
-let h1 = $('#hola_mundo');
-let button = $('#cambiar_texto');
-
-button.addEventListener('click', () => {
-	let texto = h1.innerText;
-
-	if (texto === 'Hola Mundo') {
-		h1.innerText = 'Adiós Mundo';
-	} else {
-		h1.innerText = 'Hola Mundo';
-	}
-});
 
 // Aquí empieza la hackaton semana 11
 
@@ -61,3 +42,68 @@ button.addEventListener('click', () => {
 
 // Respecto al DOM lo usarán para pintar la lista de tareas en pantalla
 // Respecto a eventos, analicen que eventos tienen que usar y apliquenlo
+
+const listaDeTareas = [
+	{
+		id: 'Tarea Nro.1',
+		titulo: 'Ir de compras',
+		descripcion: 'Ir a ubicación 1, ir a ubicación 2, ir a ubicación 3',
+		completado: false,
+		fecha: '10/11/2023',
+	},
+	{
+		id: 'Tarea Nro.2',
+		titulo: 'Asistir a clases',
+		descripcion: 'Revisar horario, buscar cuadernos, alistar mochila',
+		completado: true,
+		fecha: '20/7/2023',
+	},
+	{
+		id: 'Tarea Nro.3',
+		titulo: 'Completar avance de trabajo',
+		descripcion:
+			'Revisar estado de trabajo, buscar información, avanzar tarea pendiente',
+		completado: true,
+		fecha: '21/7/2023',
+	},
+];
+
+const stringify = JSON.stringify(listaDeTareas);
+
+function guardarDatosEnElLocalStorage(key, value) {
+	window.localStorage.setItem(key, value);
+}
+
+const key = 'lista001';
+
+guardarDatosEnElLocalStorage(key, stringify);
+
+function obtenerDatosDelLocalStorage(key) {
+	return window.localStorage.getItem(key);
+}
+
+// Modificar el texto del H1
+let listadoDeTareas = $('#listado_de_tareas');
+let button = $('#mostrar_tareas');
+
+// id: 'Tarea Nro.3'
+// titulo: 'Completar avance de trabajo'
+// descripcion: 'Revisar estado de trabajo, buscar información, avanzar tarea pendiente'
+// completado: true
+// fecha: '21/7/2023'
+
+button.addEventListener('click', () => {
+	const tareas = JSON.parse(obtenerDatosDelLocalStorage(key));
+	let html;
+
+	tareas.map(({ titulo, descripcion, completado, fecha }, key) => {
+		html += `<div>
+		<h1>${titulo}</h1>
+		<p>${descripcion}</p>
+		<span>${completado ? 'Completado' : 'No completado'}</span>
+		<p>${fecha}</p>
+		</div>`;
+	});
+
+	listadoDeTareas.innerHTML = html;
+});
