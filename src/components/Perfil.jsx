0fd8b9f628074b741095b217cb5f392fd8ba3b6a
Shair17 <jimmy_jhair17@hotmail.com>
2023-08-19 17:43:50 -0500

    manejo de estado con useState y manejo de ciclo de vida con useEffect

--- src/components/Perfil.jsx
+++ src/components/Perfil.jsx
@@ -1,13 +1,31 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 // props -> propiedades
 export const Perfil = ({ nombre }) => {
+	const [nombreEsVisible, setNombreEsVisible] = useState(true);
+
+	const manejarMostrarNombre = () => {
+		setNombreEsVisible(!nombreEsVisible);
+	};
+
+	useEffect(() => {
+		if (nombreEsVisible) {
+			console.log(`el componente de nombre ${nombre} es visible`);
+		} else {
+			console.log(`el componente de nombre ${nombre} está oculto`);
+		}
+	}, [nombreEsVisible]);
+
 	return (
-		<div>
-			<img src={`https://picsum.photos/200?nombre=${nombre}`} alt={nombre} />
+		<div className="perfil-contenedor">
+			<img src={`https://picsum.photos/1000?nombre=${nombre}`} alt={nombre} />
+
+			<button onClick={manejarMostrarNombre}>
+				{nombreEsVisible ? 'Ocultar nombre' : 'Mostrar nombre'}
+			</button>
 
 			{/** ... */}
-			<p>{nombre}</p>
+			{nombreEsVisible ? <p>{nombre}</p> : null}
 		</div>
 	);
 };
